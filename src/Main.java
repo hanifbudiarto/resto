1fe1b88d8993a68d504eab48c0d6558653a067ca
hanif <m.hanifbudiarto@gmail.com>
2016-01-28 16:11:30 +0700

    edit dikit

--- src/Main.java
+++ src/Main.java
@@ -31,7 +31,6 @@ public class Main extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
